6e41ee505d648e20582de565c2b60ca589614250
Phyllis Kan <pyk9q7@gmail.com>
2019-07-18 13:28:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,27 @@
-def using_push 
+def using_push(array)
+  array.push
+end
+
+def using_unshift(array)
+
+end
+
+def using_pop(array)
+
+end
+
+def pop_with_args(array)
+
+end
+
+def using_shift(array)
+
+end
+
+def shift_with_args(array)
+
+end
+
+def using_concat(array)
 
 end
